208f015fc8875357ff39d28c1fdf3e8941193c57
Marilia Mendonça <marilianobre1@gmail.com>
2021-08-12 20:56:31 -0300

    Implementando entidades e comandos.

---
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -0,0 +1,36 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class UpdateTodoCommand : Notifiable, ICommand
+    {
+        public UpdateTodoCommand()
+        {
+        }
+
+        public UpdateTodoCommand(string title, DateTime date, string user)
+        {
+            Title = title;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa")
+                .HasMinLen(User, 6, "User", "Usuário inválido")
+            );
+        }
+    }
+}
