5d01ea9971932c1c2effaad10f975241083605e4
Marilia Mendonça <marilianobre1@gmail.com>
2021-08-12 22:52:02 -0300

    Começo do handler

---
+++ Todo.Domain/Handler/Contracts/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handler.Contracts
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
